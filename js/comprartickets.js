3d4854691d4c00092752f054269553bc978ffd76
MaEugeniaB <eugebava@gmail.com>
2022-11-01 15:41:49 -0300

    Proyecto con pag compra de tickets con alertas y resumen

--- js/comprartickets.js
+++ js/comprartickets.js
@@ -1,6 +1,8 @@
 const valorTicket = 200;
 
-
+const emailValido = mail => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+} 
 
 let descuentoEstudiante = 80;
 let descuentoTrainee = 50;
@@ -12,74 +14,66 @@ let mail = document.getElementById("mail");
 let cantidadTickets = document.getElementById("cantidadTickets");
 let categoria = document.getElementById("categoria");
 
-//let TotalValorTickets = 0;
 
 function quitarClaseError () {
     let x = document.querySelectorAll(".form-control, .form-select");
     let i;
     for (i=0; i < x.length; i++) {
-        x[i].classlist.remove ("is-invalid");
+        x[i].classList.remove("is-invalid");
     }
 }
 
-const emailValido = mail => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
-} 
-
-
 function ResetMontoTotal() {
-  //  quitarClaseError();
+    quitarClaseError();
     Total.innerHTML = "";
 }
 
 function MontoTotal (){
 
-    //quitarClaseError();
+    quitarClaseError();
 
     if (nombre.value === "") {
-       // nombre.classList.add("is-invalid");
+        nombre.classList.add ("is-invalid");
         nombre.focus();
         alert ("Por favor, escribí tu nombre");
         return;
     }
-
+    
     if (apellido.value === ""){
-        alert ("Por favor, escribí tu apellido");
         apellido.focus();
-        //apellido.classList.add("is-invalid");
+        apellido.classList.add("is-invalid");
+        alert ("Por favor, escribí tu apellido");
         return;
     }
-
+    
     if (mail.value === ""){
+        mail.focus();
+        mail.classList.add("is-invalid");
         alert ("Por favor, escribí tu mail");
-        mail.focus();
-       // mail.classList.add("is-invalid");
         return;
     }
     
-
     if (!emailValido(mail.value)) {
-        alert ("Por favor, escribí un mail válido");
         mail.focus();
-        //mail.classList.add("is-invalid");
+        mail.classList.add("is-invalid");
+        alert ("Por favor, escribí un mail válido");
         return;
     }
     
-
     if (cantidadTickets.value == 0) {
-        alert ("Por favor, elegí la cantidad de tickets");
         cantidadTickets.focus();
-       // cantidadTickets.classList.add("is-invalid");
+        cantidadTickets.classList.add("is-invalid");
+        alert ("Por favor, elegí la cantidad de tickets");
         return; 
     }
 
     if (categoria.value === "") {
-        alert ("Por favor, elegi una categoría");
         categoria.focus();
-        //categoria.classList.add("is-invalid");
+        categoria.classList.add("is-invalid");
+        alert ("Por favor, elegi una categoría");
         return;
     }
-
+   
     let TotalValorTickets = (cantidadTickets.value)*(valorTicket);
 
     if (categoria.value == 0) {
